465cca5ea515b1280199fff9616b5cad9c5e48ff
Darshan v <darshan99015@gmail.com>
2026-03-30 17:04:35 +0530

    Added demo.java file

---
+++ demo.java
@@ -0,0 +1,9 @@
+class demo{
+    public static void main(String[] args) {
+        
+    }
+
+    static  int sum(int a , int b){ return a+b; }
+
+    static int diff(int a, int b){ return a-b; }
+}
